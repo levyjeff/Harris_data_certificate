Corrected sample size formula

so_dev_survey.py
@@ -18,7 +18,7 @@ df['Data career'] = df[data_devs].apply(lambda row: 1 if sum(row) > 0 else 0, ax
 df['In US'] = df['Country'].map(lambda c: 1 if c == 'United States' else 0)
 
 results = df.groupby('Data career').sum()[languages+['Both languages', 'Neither language']]
-results['Sample size'] = results[languages+['Neither language']].sum(axis=1)
+results['Sample size'] = results[languages+['Neither language']].sum(axis=1) - results['Both languages']
 
 summary = pd.concat([results.applymap(lambda v: f'{v:,}'),
                      results.apply(lambda row: row / row['Sample size'], axis=1).applymap(lambda v: f'{v:.1%}')
@@ -26,18 +26,18 @@ summary = pd.concat([results.applymap(lambda v: f'{v:,}'),
 
 summary.index = summary.index.map(lambda i: 'Yes' if i == 1 else 'No')
 
-# In [64]: summary
-# Out[64]:
-#                R  Python Both languages Neither language Sample size
+# In [8]: summary
+# Out[8]:
+#                  R  Python Both languages Neither language Sample size
 # Data career
-# Yes        3,133  10,473          2,483            6,012      19,618
-# Yes        16.0%   53.4%          12.7%            30.6%      100.0%
-# No         1,915  25,970          1,367           45,230      73,115
-# No          2.6%   35.5%           1.9%            61.9%      100.0%
+# Yes          3,133  10,473          2,483            6,012      17,135
+# Yes          18.3%   61.1%          14.5%            35.1%      100.0%
+# No           1,915  25,970          1,367           45,230      71,748
+# No            2.7%   36.2%           1.9%            63.0%      100.0%
 
-# In [69]: df[(df['Neither language'] == 1) & (df['Data career'] == 1)]['LanguageWorkedWith'].
-#     ...: str.get_dummies(sep=';').sum().sort_values(ascending=False)
-# Out[69]:
+# In [12]: df[(df['Neither language'] == 1) & (df['Data career'] == 1)]['LanguageWorkedWith'
+#     ...: ].str.get_dummies(sep=';').sum().sort_values(ascending=False)
+# Out[12]:
 # SQL                      3766
 # JavaScript               3722
 # HTML/CSS                 3657